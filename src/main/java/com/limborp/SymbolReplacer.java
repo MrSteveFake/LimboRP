4b68d211fdf23087a794f3788e8315b48a1ac17b
MrSteveFake <biazermine@mail.ru>
2026-07-05 14:27:17 +0300

    Update SymbolReplacer.java

--- src/main/java/com/limborp/SymbolReplacer.java
+++ src/main/java/com/limborp/SymbolReplacer.java
@@ -1,173 +1,201 @@
 package com.limborp;
 
+import org.bukkit.ChatColor;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SymbolReplacer {
     
     private final LimboRP plugin;
-    private final Map<Character, Character> symbolToLetter;
+    private final Map<Character, String> symbolToLetter;
+    private final Map<String, String> wordReplacements;
     
     public SymbolReplacer(LimboRP plugin) {
         this.plugin = plugin;
         this.symbolToLetter = new HashMap<>();
+        this.wordReplacements = new HashMap<>();
         
         // Загружаем замены
         loadReplacements();
     }
     
     private void loadReplacements() {
-        // Русские буквы
-        symbolToLetter.put('\u0410', 'А'); // А
-        symbolToLetter.put('\u0411', 'Б'); // Б
-        symbolToLetter.put('\u0412', 'В'); // В
-        symbolToLetter.put('\u0413', 'Г'); // Г
-        symbolToLetter.put('\u0414', 'Д'); // Д
-        symbolToLetter.put('\u0415', 'Е'); // Е
-        symbolToLetter.put('\u0401', 'Ё'); // Ё
-        symbolToLetter.put('\u0416', 'Ж'); // Ж
-        symbolToLetter.put('\u0417', 'З'); // З
-        symbolToLetter.put('\u0418', 'И'); // И
-        symbolToLetter.put('\u0419', 'Й'); // Й
-        symbolToLetter.put('\u041A', 'К'); // К
-        symbolToLetter.put('\u041B', 'Л'); // Л
-        symbolToLetter.put('\u041C', 'М'); // М
-        symbolToLetter.put('\u041D', 'Н'); // Н
-        symbolToLetter.put('\u041E', 'О'); // О
-        symbolToLetter.put('\u041F', 'П'); // П
-        symbolToLetter.put('\u0420', 'Р'); // Р
-        symbolToLetter.put('\u0421', 'С'); // С
-        symbolToLetter.put('\u0422', 'Т'); // Т
-        symbolToLetter.put('\u0423', 'У'); // У
-        symbolToLetter.put('\u0424', 'Ф'); // Ф
-        symbolToLetter.put('\u0425', 'Х'); // Х
-        symbolToLetter.put('\u0426', 'Ц'); // Ц
-        symbolToLetter.put('\u0427', 'Ч'); // Ч
-        symbolToLetter.put('\u0428', 'Ш'); // Ш
-        symbolToLetter.put('\u0429', 'Щ'); // Щ
-        symbolToLetter.put('\u042A', 'Ъ'); // Ъ
-        symbolToLetter.put('\u042B', 'Ы'); // Ы
-        symbolToLetter.put('\u042C', 'Ь'); // Ь
-        symbolToLetter.put('\u042D', 'Э'); // Э
-        symbolToLetter.put('\u042E', 'Ю'); // Ю
-        symbolToLetter.put('\u042F', 'Я'); // Я
+        // Стандартные замены символов
+        symbolToLetter.clear();
         
-        symbolToLetter.put('\u0430', 'а'); // а
-        symbolToLetter.put('\u0431', 'б'); // б
-        symbolToLetter.put('\u0432', 'в'); // в
-        symbolToLetter.put('\u0433', 'г'); // г
-        symbolToLetter.put('\u0434', 'д'); // д
-        symbolToLetter.put('\u0435', 'е'); // е
-        symbolToLetter.put('\u0451', 'ё'); // ё
-        symbolToLetter.put('\u0436', 'ж'); // ж
-        symbolToLetter.put('\u0437', 'з'); // з
-        symbolToLetter.put('\u0438', 'и'); // и
-        symbolToLetter.put('\u0439', 'й'); // й
-        symbolToLetter.put('\u043A', 'к'); // к
-        symbolToLetter.put('\u043B', 'л'); // л
-        symbolToLetter.put('\u043C', 'м'); // м
-        symbolToLetter.put('\u043D', 'н'); // н
-        symbolToLetter.put('\u043E', 'о'); // о
-        symbolToLetter.put('\u043F', 'п'); // п
-        symbolToLetter.put('\u0440', 'р'); // р
-        symbolToLetter.put('\u0441', 'с'); // с
-        symbolToLetter.put('\u0442', 'т'); // т
-        symbolToLetter.put('\u0443', 'у'); // у
-        symbolToLetter.put('\u0444', 'ф'); // ф
-        symbolToLetter.put('\u0445', 'х'); // х
-        symbolToLetter.put('\u0446', 'ц'); // ц
-        symbolToLetter.put('\u0447', 'ч'); // ч
-        symbolToLetter.put('\u0448', 'ш'); // ш
-        symbolToLetter.put('\u0449', 'щ'); // щ
-        symbolToLetter.put('\u044A', 'ъ'); // ъ
-        symbolToLetter.put('\u044B', 'ы'); // ы
-        symbolToLetter.put('\u044C', 'ь'); // ь
-        symbolToLetter.put('\u044D', 'э'); // э
-        symbolToLetter.put('\u044E', 'ю'); // ю
-        symbolToLetter.put('\u044F', 'я'); // я
+        // Русские буквы
+        symbolToLetter.put('А', "А");
+        symbolToLetter.put('Б', "Б");
+        symbolToLetter.put('В', "В");
+        symbolToLetter.put('Г', "Г");
+        symbolToLetter.put('Д', "Д");
+        symbolToLetter.put('Е', "Е");
+        symbolToLetter.put('Ё', "Ё");
+        symbolToLetter.put('Ж', "Ж");
+        symbolToLetter.put('З', "З");
+        symbolToLetter.put('И', "И");
+        symbolToLetter.put('Й', "Й");
+        symbolToLetter.put('К', "К");
+        symbolToLetter.put('Л', "Л");
+        symbolToLetter.put('М', "М");
+        symbolToLetter.put('Н', "Н");
+        symbolToLetter.put('О', "О");
+        symbolToLetter.put('П', "П");
+        symbolToLetter.put('Р', "Р");
+        symbolToLetter.put('С', "С");
+        symbolToLetter.put('Т', "Т");
+        symbolToLetter.put('У', "У");
+        symbolToLetter.put('Ф', "Ф");
+        symbolToLetter.put('Х', "Х");
+        symbolToLetter.put('Ц', "Ц");
+        symbolToLetter.put('Ч', "Ч");
+        symbolToLetter.put('Ш', "Ш");
+        symbolToLetter.put('Щ', "Щ");
+        symbolToLetter.put('Ъ', "Ъ");
+        symbolToLetter.put('Ы', "Ы");
+        symbolToLetter.put('Ь', "Ь");
+        symbolToLetter.put('Э', "Э");
+        symbolToLetter.put('Ю', "Ю");
+        symbolToLetter.put('Я', "Я");
+        
+        symbolToLetter.put('а', "а");
+        symbolToLetter.put('б', "б");
+        symbolToLetter.put('в', "в");
+        symbolToLetter.put('г', "г");
+        symbolToLetter.put('д', "д");
+        symbolToLetter.put('е', "е");
+        symbolToLetter.put('ё', "ё");
+        symbolToLetter.put('ж', "ж");
+        symbolToLetter.put('з', "з");
+        symbolToLetter.put('и', "и");
+        symbolToLetter.put('й', "й");
+        symbolToLetter.put('к', "к");
+        symbolToLetter.put('л', "л");
+        symbolToLetter.put('м', "м");
+        symbolToLetter.put('н', "н");
+        symbolToLetter.put('о', "о");
+        symbolToLetter.put('п', "п");
+        symbolToLetter.put('р', "р");
+        symbolToLetter.put('с', "с");
+        symbolToLetter.put('т', "т");
+        symbolToLetter.put('у', "у");
+        symbolToLetter.put('ф', "ф");
+        symbolToLetter.put('х', "х");
+        symbolToLetter.put('ц', "ц");
+        symbolToLetter.put('ч', "ч");
+        symbolToLetter.put('ш', "ш");
+        symbolToLetter.put('щ', "щ");
+        symbolToLetter.put('ъ', "ъ");
+        symbolToLetter.put('ы', "ы");
+        symbolToLetter.put('ь', "ь");
+        symbolToLetter.put('э', "э");
+        symbolToLetter.put('ю', "ю");
+        symbolToLetter.put('я', "я");
         
         // Английские буквы
-        symbolToLetter.put('\uFF21', 'A'); // Ａ
-        symbolToLetter.put('\uFF22', 'B'); // Ｂ
-        symbolToLetter.put('\uFF23', 'C'); // Ｃ
-        symbolToLetter.put('\uFF24', 'D'); // Ｄ
-        symbolToLetter.put('\uFF25', 'E'); // Ｅ
-        symbolToLetter.put('\uFF26', 'F'); // Ｆ
-        symbolToLetter.put('\uFF27', 'G'); // Ｇ
-        symbolToLetter.put('\uFF28', 'H'); // Ｈ
-        symbolToLetter.put('\uFF29', 'I'); // Ｉ
-        symbolToLetter.put('\uFF2A', 'J'); // Ｊ
-        symbolToLetter.put('\uFF2B', 'K'); // Ｋ
-        symbolToLetter.put('\uFF2C', 'L'); // Ｌ
-        symbolToLetter.put('\uFF2D', 'M'); // Ｍ
-        symbolToLetter.put('\uFF2E', 'N'); // Ｎ
-        symbolToLetter.put('\uFF2F', 'O'); // Ｏ
-        symbolToLetter.put('\uFF30', 'P'); // Ｐ
-        symbolToLetter.put('\uFF31', 'Q'); // Ｑ
-        symbolToLetter.put('\uFF32', 'R'); // Ｒ
-        symbolToLetter.put('\uFF33', 'S'); // Ｓ
-        symbolToLetter.put('\uFF34', 'T'); // Ｔ
-        symbolToLetter.put('\uFF35', 'U'); // Ｕ
-        symbolToLetter.put('\uFF36', 'V'); // Ｖ
-        symbolToLetter.put('\uFF37', 'W'); // Ｗ
-        symbolToLetter.put('\uFF38', 'X'); // Ｘ
-        symbolToLetter.put('\uFF39', 'Y'); // Ｙ
-        symbolToLetter.put('\uFF3A', 'Z'); // Ｚ
+        symbolToLetter.put('A', "A");
+        symbolToLetter.put('B', "B");
+        symbolToLetter.put('C', "C");
+        symbolToLetter.put('D', "D");
+        symbolToLetter.put('E', "E");
+        symbolToLetter.put('F', "F");
+        symbolToLetter.put('G', "G");
+        symbolToLetter.put('H', "H");
+        symbolToLetter.put('I', "I");
+        symbolToLetter.put('J', "J");
+        symbolToLetter.put('K', "K");
+        symbolToLetter.put('L', "L");
+        symbolToLetter.put('M', "M");
+        symbolToLetter.put('N', "N");
+        symbolToLetter.put('O', "O");
+        symbolToLetter.put('P', "P");
+        symbolToLetter.put('Q', "Q");
+        symbolToLetter.put('R', "R");
+        symbolToLetter.put('S', "S");
+        symbolToLetter.put('T', "T");
+        symbolToLetter.put('U', "U");
+        symbolToLetter.put('V', "V");
+        symbolToLetter.put('W', "W");
+        symbolToLetter.put('X', "X");
+        symbolToLetter.put('Y', "Y");
+        symbolToLetter.put('Z', "Z");
         
-        symbolToLetter.put('\uFF41', 'a'); // ａ
-        symbolToLetter.put('\uFF42', 'b'); // ｂ
-        symbolToLetter.put('\uFF43', 'c'); // ｃ
-        symbolToLetter.put('\uFF44', 'd'); // ｄ
-        symbolToLetter.put('\uFF45', 'e'); // ｅ
-        symbolToLetter.put('\uFF46', 'f'); // ｆ
-        symbolToLetter.put('\uFF47', 'g'); // ｇ
-        symbolToLetter.put('\uFF48', 'h'); // ｈ
-        symbolToLetter.put('\uFF49', 'i'); // ｉ
-        symbolToLetter.put('\uFF4A', 'j'); // ｊ
-        symbolToLetter.put('\uFF4B', 'k'); // ｋ
-        symbolToLetter.put('\uFF4C', 'l'); // ｌ
-        symbolToLetter.put('\uFF4D', 'm'); // ｍ
-        symbolToLetter.put('\uFF4E', 'n'); // ｎ
-        symbolToLetter.put('\uFF4F', 'o'); // ｏ
-        symbolToLetter.put('\uFF50', 'p'); // ｐ
-        symbolToLetter.put('\uFF51', 'q'); // ｑ
-        symbolToLetter.put('\uFF52', 'r'); // ｒ
-        symbolToLetter.put('\uFF53', 's'); // ｓ
-        symbolToLetter.put('\uFF54', 't'); // ｔ
-        symbolToLetter.put('\uFF55', 'u'); // ｕ
-        symbolToLetter.put('\uFF56', 'v'); // ｖ
-        symbolToLetter.put('\uFF57', 'w'); // ｗ
-        symbolToLetter.put('\uFF58', 'x'); // ｘ
-        symbolToLetter.put('\uFF59', 'y'); // ｙ
-        symbolToLetter.put('\uFF5A', 'z'); // ｚ
+        symbolToLetter.put('a', "a");
+        symbolToLetter.put('b', "b");
+        symbolToLetter.put('c', "c");
+        symbolToLetter.put('d', "d");
+        symbolToLetter.put('e', "e");
+        symbolToLetter.put('f', "f");
+        symbolToLetter.put('g', "g");
+        symbolToLetter.put('h', "h");
+        symbolToLetter.put('i', "i");
+        symbolToLetter.put('j', "j");
+        symbolToLetter.put('k', "k");
+        symbolToLetter.put('l', "l");
+        symbolToLetter.put('m', "m");
+        symbolToLetter.put('n', "n");
+        symbolToLetter.put('o', "o");
+        symbolToLetter.put('p', "p");
+        symbolToLetter.put('q', "q");
+        symbolToLetter.put('r', "r");
+        symbolToLetter.put('s', "s");
+        symbolToLetter.put('t', "t");
+        symbolToLetter.put('u', "u");
+        symbolToLetter.put('v', "v");
+        symbolToLetter.put('w', "w");
+        symbolToLetter.put('x', "x");
+        symbolToLetter.put('y', "y");
+        symbolToLetter.put('z', "z");
         
         // Цифры
-        symbolToLetter.put('\uFF10', '0'); // ０
-        symbolToLetter.put('\uFF11', '1'); // １
-        symbolToLetter.put('\uFF12', '2'); // ２
-        symbolToLetter.put('\uFF13', '3'); // ３
-        symbolToLetter.put('\uFF14', '4'); // ４
-        symbolToLetter.put('\uFF15', '5'); // ５
-        symbolToLetter.put('\uFF16', '6'); // ６
-        symbolToLetter.put('\uFF17', '7'); // ７
-        symbolToLetter.put('\uFF18', '8'); // ８
-        symbolToLetter.put('\uFF19', '9'); // ９
+        symbolToLetter.put('0', "0");
+        symbolToLetter.put('1', "1");
+        symbolToLetter.put('2', "2");
+        symbolToLetter.put('3', "3");
+        symbolToLetter.put('4', "4");
+        symbolToLetter.put('5', "5");
+        symbolToLetter.put('6', "6");
+        symbolToLetter.put('7', "7");
+        symbolToLetter.put('8', "8");
+        symbolToLetter.put('9', "9");
         
         // Специальные символы
-        symbolToLetter.put('\uFF0B', '+'); // ＋
-        symbolToLetter.put('\uFF0D', '-'); // －
-        symbolToLetter.put('\uFF1D', '='); // ＝
-        symbolToLetter.put('\uFF08', '('); // （
-        symbolToLetter.put('\uFF09', ')'); // ）
-        symbolToLetter.put('\uFF20', '@'); // ＠
-        symbolToLetter.put('\uFF01', '!'); // ！
-        symbolToLetter.put('\uFF1F', '?'); // ？
-        symbolToLetter.put('\uFF03', '#'); // ＃
-        symbolToLetter.put('\uFF04', '$'); // ＄
-        symbolToLetter.put('\uFF05', '%'); // ％
-        symbolToLetter.put('\uFF3E', '^'); // ＾
-        symbolToLetter.put('\uFF06', '&'); // ＆
-        symbolToLetter.put('\uFF0A', '*'); // ＊
+        symbolToLetter.put('!', "!");
+        symbolToLetter.put('?', "?");
+        symbolToLetter.put('.', ".");
+        symbolToLetter.put(',', ",");
+        symbolToLetter.put(':', ":");
+        symbolToLetter.put(';', ";");
+        symbolToLetter.put('-', "-");
+        symbolToLetter.put('+', "+");
+        symbolToLetter.put('=', "=");
+        symbolToLetter.put('(', "(");
+        symbolToLetter.put(')', ")");
+        symbolToLetter.put('[', "[");
+        symbolToLetter.put(']', "]");
+        symbolToLetter.put('{', "{");
+        symbolToLetter.put('}', "}");
+        symbolToLetter.put('<', "<");
+        symbolToLetter.put('>', ">");
+        symbolToLetter.put('/', "/");
+        symbolToLetter.put('\\', "\\");
+        symbolToLetter.put('|', "|");
+        symbolToLetter.put('@', "@");
+        symbolToLetter.put('#', "#");
+        symbolToLetter.put('$', "$");
+        symbolToLetter.put('%', "%");
+        symbolToLetter.put('^', "^");
+        symbolToLetter.put('&', "&");
+        symbolToLetter.put('*', "*");
+        symbolToLetter.put('_', "_");
+        symbolToLetter.put('~', "~");
+        symbolToLetter.put('`', "`");
+        symbolToLetter.put('\'', "'");
+        symbolToLetter.put('"', "\"");
+        symbolToLetter.put(' ', " ");
     }
     
     public String replaceSymbols(String text) {
@@ -177,8 +205,15 @@ public class SymbolReplacer {
         
         StringBuilder result = new StringBuilder();
         
-        for (char c : text.toCharArray()) {
-            Character replacement = symbolToLetter.get(c);
+        // Сначала проверяем замены слов/фраз
+        String processedText = text;
+        for (Map.Entry<String, String> entry : wordReplacements.entrySet()) {
+            processedText = processedText.replace(entry.getKey(), entry.getValue());
+        }
+        
+        // Затем заменяем отдельные символы
+        for (char c : processedText.toCharArray()) {
+            String replacement = symbolToLetter.get(c);
             if (replacement != null) {
                 result.append(replacement);
             } else {
@@ -190,17 +225,34 @@ public class SymbolReplacer {
     }
     
     public void reloadReplacements() {
-        symbolToLetter.clear();
         loadReplacements();
         
-        // Если есть дополнительные замены в конфиге
+        // Загружаем дополнительные замены из конфига
         if (plugin.getConfig().contains("replacements")) {
             for (String key : plugin.getConfig().getConfigurationSection("replacements").getKeys(false)) {
                 String value = plugin.getConfig().getString("replacements." + key);
-                if (key.length() == 1 && value != null && value.length() == 1) {
-                    symbolToLetter.put(key.charAt(0), value.charAt(0));
+                
+                if (key.length() == 1 && value != null) {
+                    // Если ключ - один символ, а значение - строка
+                    if (value.length() == 1) {
+                        // Простая замена символ на символ
+                        symbolToLetter.put(key.charAt(0), value);
+                    } else {
+                        // Замена символа на строку (с поддержкой цветов)
+                        symbolToLetter.put(key.charAt(0), 
+                            ChatColor.translateAlternateColorCodes('&', value));
+                    }
+                } else if (key.length() > 1 && value != null) {
+                    // Замена слова/фразы
+                    wordReplacements.put(key, 
+                        ChatColor.translateAlternateColorCodes('&', value));
                 }
             }
+        }
+        
+        if (plugin.getConfig().getBoolean("debug", false)) {
+            plugin.getLogger().info("Loaded " + symbolToLetter.size() + " symbol replacements");
+            plugin.getLogger().info("Loaded " + wordReplacements.size() + " word replacements");
         }
     }
 }
